60e0eacf46df3cd34dc7a36ab19b87fbeb16d887
Ilya Svyatski <promonolit1234567@gmail.com>
2022-11-01 10:33:10 +0300

    All

---
+++ Pods/Target Support Files/CustomizableTabBar/CustomizableTabBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CustomizableTabBar : NSObject
+@end
+@implementation PodsDummy_CustomizableTabBar
+@end
---
+++ Pods/Target Support Files/Pods-KanarieTask-KanarieTaskUITests/Pods-KanarieTask-KanarieTaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KanarieTask_KanarieTaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KanarieTask_KanarieTaskUITests
+@end
